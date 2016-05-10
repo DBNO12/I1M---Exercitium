237aa46bc945074685e72f0b29b2796e4263119e
DBNO12 <francz97@hotmail.com>
2016-05-10 22:36:52 +0200

    http://www.glc.us.es/~jalonso/exercitium/sucesion-fractal

--- Exercitium-100516.hs
+++ Exercitium-100516.hs
@@ -4,3 +4,7 @@ sucFractal = 0: fractal
 
 sumaSucFractal :: Integer -> Integer
 sumaSucFractal = sum . flip take sucFractal . fromIntegral
+
+sucFractal2 :: [Integer]
+sucFractal2 = 0: fractal
+  where fractal = 0: (concat $ zipWith (\x y -> [x,y]) [1..] fractal)
