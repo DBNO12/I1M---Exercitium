5ce80c8a8536a414e887cbd20c335a2677291d90
DBNO12 <francz97@hotmail.com>
2016-05-10 22:28:35 +0200

    http://www.glc.us.es/~jalonso/exercitium/sucesion-fractal/

--- Exercitium-100516.hs
+++ Exercitium-100516.hs
@@ -1,6 +1,6 @@
 sucFractal :: [Integer]
-sucFractal =
-    tail $ concat [[x,y] | (x,y) <- zip (0:sucFractal) [0..]]
+sucFractal = 0: fractal
+  where fractal = 0: concat [[x,y] | (x,y) <- zip [1..] fractal]
 
 sumaSucFractal :: Integer -> Integer
 sumaSucFractal = sum . flip take sucFractal . fromIntegral
