62ddfe3841fca13e35b3e945e8ef6b03c11a64fa
DBNO12 <francz97@hotmail.com>
2016-05-14 22:30:03 +0200

    http://www.glc.us.es/~jalonso/exercitium/problema-de-las-puertas

--- Exercitium-120516.hs
+++ Exercitium-120516.hs
@@ -1,34 +1,19 @@
-import Data.Maybe
-import Data.Char
+import Data.Numbers.Primes
 import Data.List
 
-soluciones :: [String] -> String -> [[String]]
-soluciones fs ls = soluciona [([],ls,fs)]
-                   
-soluciona :: [([String],String,[String])] -> [[String]]
-soluciona ((ps,(q:qs),rs):xs)
-    | isNothing e = soluciona xs
-    | otherwise   =
-        soluciona (xs ++ [(ys:ps, qs, delete ys rs) |
-                                        ys <- fromJust e])
-  where e = encuentra q rs
-soluciona ((ps,[],_):xs) = reverse ps : soluciona xs
-soluciona   _            = []
-   -- Cada terna representa:
-   --     (Fichas usadas en orden invero,
-   --         letras restantes que buscar,
-   --            batería de fichas donde buscar)
-                           
-encuentra :: Char -> [String] -> Maybe [String]
-encuentra x rs | null xs   = Nothing
-               | otherwise = Just xs
-  where xs = filter (pertenece x) rs
-   -- λ> encuentra 'x' ["HO","LA"]  λ> encuentra 'A' ["HO","LA"]
-   -- Nothing                       Just ["LA"]
-             
-pertenece :: Char -> String -> Bool
-pertenece x xs = xm `elem` xs || xM `elem` xs
-  where xm = toLower x
-        xM = toUpper x
-   -- λ> pertenece 'h' "HOLA"  λ> pertenece 'h' "7OLA"
-   -- True                     False
+data Estado = Abierta | Cerrada 
+  deriving (Eq, Show)
+ 
+final :: Int -> [Estado]
+final 0 = [Cerrada]
+final n = Abierta: checkEstado 2 n
+  where checkEstado :: Int -> Int -> [Estado]
+        checkEstado m n 
+            | m > n     = []
+            | otherwise = paridad l : checkEstado (m+1) n
+          where l = product [length xs + 1 |
+                             xs <- group $ primeFactors m]
+        paridad :: Int -> Estado
+        paridad l | even l    = Cerrada
+                  | otherwise = Abierta
+
